c684de0ec5b5ab17300b5f04fa68f0671e59c17b
Kishore <kishorekumarsaran2001@gmail.com>
2023-11-17 12:33:04 +0000

    Access Database Via Php

--- libs/load.php
+++ libs/load.php
@@ -2,7 +2,7 @@
 
 function load_template($name)
 {
-    include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php"; //not consistant.
+    include $_SERVER['DOCUMENT_ROOT'] . "/_templates/$name.php"; //not consistant.
 }
 
 function validate_credentials($username, $password)
@@ -12,4 +12,32 @@ function validate_credentials($username, $password)
     } else {
         return false;
     }
+}
+
+function signup($user, $pass, $email, $phone)
+{
+    $servername = "mysql.selfmade.ninja";
+    $username = "Kishore2071";
+    $password = "qH@kwc8HdTKe5QK";
+    $dbname = "Kishore2071_newdb";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `active`)
+    VALUES ('$user', '$pass', '$email', '$phone', '1');";
+    $error = false;
+    if ($conn->query($sql) === true) {
+        $error = false;
+    } else {
+        // echo "Error: " . $sql . "<br>" . $conn->error;
+        $error = $conn->error;
+    }
+
+    $conn->close();
+    return $error;
 }
--- login.php
+++ login.php
@@ -6,92 +6,18 @@ include 'libs/load.php';
 
 <!doctype html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Selfmade Ninja Academy">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Login to Photogram</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css"
-        integrity="sha512-glciccPoOqr5mfDGmlJ3bpbvomZmFK+5dRARpt62nZnlKwaYZSfFpFIgUoD8ujqBw4TmPa/F3TX28OctJzoLfg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        .form-signin {
-            width: 100%;
-            max-width: 330px;
-            padding: 15px;
-            margin: auto;
-        }
-
-        .form-signin .checkbox {
-            font-weight: 400;
-        }
-
-        .form-signin .form-floating:focus-within {
-            z-index: 2;
-        }
-
-        .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-        }
-
-        .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-        }
-    </style>
-</head>
+<?load_template('_head');?>
 
 <body>
 
-    <header>
-        <div class="collapse bg-dark" id="navbarHeader">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-8 col-md-7 py-4">
-                        <h4 class="text-white">About</h4>
-                        <p class="text-muted">Add some information about the album below, the author, or any other
-                            background
-                            context. Make it a few sentences long so folks can pick up some informative tidbits. Then,
-                            link them off
-                            to some social networking sites or contact information.</p>
-                    </div>
-                    <div class="col-sm-4 offset-md-1 py-4">
-                        <h4 class="text-white">Contact</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                            <li><a href="#" class="text-white">Like on Facebook</a></li>
-                            <li><a href="#" class="text-white">Email me</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?load_template('_header');?>
-    </header>
-    <main>
+	<?load_template('_header');?>
+	<main>
 
-        <?load_template('_login');?>
+		<?load_template('_login');?>
 
-    </main>
-    <?load_template('_footer');?>
-    <script src="/app/assets/dist/js/bootstrap.bundle.min.js"></script>
+	</main>
+	<?load_template('_footer');?>
+	<script src="/assets/dist/js/bootstrap.bundle.min.js"></script>
 
 
 </body>
